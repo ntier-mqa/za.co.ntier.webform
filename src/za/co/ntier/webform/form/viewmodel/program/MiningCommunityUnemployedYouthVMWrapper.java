931080b7ec8b1c072b21f8410724af39e78dddce
martin <maugustine@ntier.co.za>
2025-09-16 10:52:06 +0200

    Mining community mandatory changes,  just temp changes.

--- src/za/co/ntier/webform/form/viewmodel/program/MiningCommunityUnemployedYouthVMWrapper.java
+++ src/za/co/ntier/webform/form/viewmodel/program/MiningCommunityUnemployedYouthVMWrapper.java
@@ -1,9 +1,28 @@
 package za.co.ntier.webform.form.viewmodel.program;
 
+import org.zkoss.bind.annotation.ExecutionArgParam;
 import org.zkoss.bind.annotation.Init;
 
 import za.co.ntier.webform.form.bean.program.MiningCommunityUnemployedYouthProgram;
-@Init(superclass = true)
+import za.co.ntier.webform.form.viewmodel.DiscretionaryGrantsApplicationProgramVM;
+
+
+
 public class MiningCommunityUnemployedYouthVMWrapper extends ProgramVMWrapper<MiningCommunityUnemployedYouthProgram>{
+	
+    private MiningCommunityUnemployedYouthProgram program;
+    private DiscretionaryGrantsApplicationProgramVM applicationProgramVM;
+
+    @Init(superclass = true)
+    public void init(
+        @ExecutionArgParam("program") MiningCommunityUnemployedYouthProgram program,
+        @ExecutionArgParam("applicationProgramVM") DiscretionaryGrantsApplicationProgramVM appVM
+    ) {
+        this.program = program;
+        this.applicationProgramVM = appVM;
+    }
+
+    public MiningCommunityUnemployedYouthProgram getProgram() { return program; }
+    public DiscretionaryGrantsApplicationProgramVM getApplicationProgramVM() { return applicationProgramVM; }
 
 }
