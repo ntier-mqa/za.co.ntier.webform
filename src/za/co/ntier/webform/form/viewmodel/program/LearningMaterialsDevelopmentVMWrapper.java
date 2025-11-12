2f4e78b7d3b66ed0ce23735b065c044817cd8da5
hieplq <hieplq@hasuvimex.vn>
2025-11-13 00:25:39 +0700

    download File AnnexureB

--- src/za/co/ntier/webform/form/viewmodel/program/LearningMaterialsDevelopmentVMWrapper.java
+++ src/za/co/ntier/webform/form/viewmodel/program/LearningMaterialsDevelopmentVMWrapper.java
@@ -1,10 +1,32 @@
 package za.co.ntier.webform.form.viewmodel.program;
 
-import org.zkoss.bind.annotation.Init;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
+import org.compiere.model.MAttachment;
+import org.compiere.model.PO;
+import org.compiere.util.Env;
+import org.idempiere.ui.zk.media.Medias;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.util.media.AMedia;
+import org.zkoss.zul.Filedownload;
+
+import za.co.ntier.api.model.X_ZZ_Program_Master_Data;
 import za.co.ntier.webform.form.bean.program.LearningMaterialsDevelopment;
 
 @Init(superclass = true)
 public class LearningMaterialsDevelopmentVMWrapper extends ProgramVMWrapper<LearningMaterialsDevelopment>{
-
+	@Command
+	public void downloadFileAnnexureB() throws IOException {
+		PO programDefine = (PO)getMenuContextInfo().getProgramMasterData();
+		if (programDefine != null) {
+			byte[] fileData = programDefine.getPdfAttachment();
+			if (fileData != null) {
+				Filedownload.save(fileData, Medias.PDF_MIME_TYPE, "AnnexureB.pdf");
+			}
+			
+		}
+	}
 }
