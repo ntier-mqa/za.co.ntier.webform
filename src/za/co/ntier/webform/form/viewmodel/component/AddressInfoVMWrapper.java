e31040c85e514c88b7d3bdd8304dd5dc5d68aa24
martin <maugustine@ntier.co.za>
2025-11-21 15:59:07 +0200

    For Alternate addresses don't force entry

--- src/za/co/ntier/webform/form/viewmodel/component/AddressInfoVMWrapper.java
+++ src/za/co/ntier/webform/form/viewmodel/component/AddressInfoVMWrapper.java
@@ -5,55 +5,67 @@ import org.compiere.model.MRegion;
 import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ExecutionArgParam;
 import org.zkoss.bind.annotation.Init;
 
 import za.co.ntier.webform.form.bean.component.AddressInfo;
-@Init(superclass = true)
 public class AddressInfoVMWrapper extends ComponentVMWrapper<AddressInfo>{
-	
+
+	private boolean relaxedConstraints = false;
+
+    @Init(superclass = true)
+    public void init(@ExecutionArgParam("relaxedConstraints") Boolean relaxedConstraints) {
+        // this will also call @Init in ComponentVMWrapper because of superclass = true
+        this.relaxedConstraints = relaxedConstraints != null && relaxedConstraints;
+    }
+
+    public boolean isRelaxedConstraints() {
+        return relaxedConstraints;
+    }
+
 	// set text fields while typing ("" -> null)
-    @Command
-    public void fieldChanging(@BindingParam("field") String field,
-                              @BindingParam("val") String val) {
-        String v = (val == null || val.trim().isEmpty()) ? null : val.trim();
-        switch (field) {
-            case "siteName":                   component.setSiteName(v); break;
-            case "addressLine":                component.setAddressLine(v); break;
-            case "postalCode":                 component.setPostalCode(v); break;
-            case "nameSiteRepresentative":     component.setNameSiteRepresentative(v); break;
-            case "representativeDesignation":  component.setRepresentativeDesignation(v); break;
-            case "mobileNumber":               component.setMobileNumber(v); break;
-            case "landlineNumber":             component.setLandlineNumber(v); break;
-            case "email":                      component.setEmail(v); break;
-        }
-        BindUtils.postNotifyChange(null, null, getApplicationProgramVM(), getNotifyTarget());
-    }
-    
-    @Command
-    public void areaSelected(@BindingParam("item") MCity item) {
-    	component.setAreaSelected(item); // matches setter signature
-        BindUtils.postNotifyChange(null, null, getApplicationProgramVM(), getNotifyTarget());
-    }
+	@Command
+	public void fieldChanging(@BindingParam("field") String field,
+			@BindingParam("val") String val) {
+		String v = (val == null || val.trim().isEmpty()) ? null : val.trim();
+		switch (field) {
+		case "siteName":                   component.setSiteName(v); break;
+		case "addressLine":                component.setAddressLine(v); break;
+		case "postalCode":                 component.setPostalCode(v); break;
+		case "nameSiteRepresentative":     component.setNameSiteRepresentative(v); break;
+		case "representativeDesignation":  component.setRepresentativeDesignation(v); break;
+		case "mobileNumber":               component.setMobileNumber(v); break;
+		case "landlineNumber":             component.setLandlineNumber(v); break;
+		case "email":                      component.setEmail(v); break;
+		}
+		BindUtils.postNotifyChange(null, null, getApplicationProgramVM(), getNotifyTarget());
+	}
 
-    @Command
-    public void provinceSelected(@BindingParam("item") MRegion item) {
-    	component.setProvinceSelected(item); // matches setter signature
-        BindUtils.postNotifyChange(null, null, getApplicationProgramVM(), getNotifyTarget());
-    }
+	@Command
+	public void areaSelected(@BindingParam("item") MCity item) {
+		component.setAreaSelected(item); // matches setter signature
+		BindUtils.postNotifyChange(null, null, getApplicationProgramVM(), getNotifyTarget());
+	}
 
-	
+	@Command
+	public void provinceSelected(@BindingParam("item") MRegion item) {
+		component.setProvinceSelected(item); // matches setter signature
+		BindUtils.postNotifyChange(null, null, getApplicationProgramVM(), getNotifyTarget());
+	}
+
+
 	@Command
 	public void notifyParentCompleteness() {
-	    if (getApplicationProgramVM() != null && getNotifyTarget() != null) {
-	        BindUtils.postNotifyChange(null, null, getApplicationProgramVM(), getNotifyTarget());
-	    }
-	    // make the TabButtonVM re-evaluate next/submit
-	    BindUtils.postGlobalCommand(null, null, "tabSelectionChanged", null);
+		if (getApplicationProgramVM() != null && getNotifyTarget() != null) {
+			BindUtils.postNotifyChange(null, null, getApplicationProgramVM(), getNotifyTarget());
+		}
+		// make the MainButtonComponentVMWrapper re-evaluate next/submit
+		BindUtils.postGlobalCommand(null, null, "tabSelectionChanged", null);
 	}
-	
+
 	@Command
 	public void copyAddress() {
 		component.copyAddress();
 	}
-	
+
 }
