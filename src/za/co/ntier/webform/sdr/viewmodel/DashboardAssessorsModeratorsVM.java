40b4ace9470d139130b8590f8fcb4d220405b8d5
hieplq <hieplq@hasuvimex.vn>
2026-08-05 13:15:09 +0700

    filter dashboard (fix parameter)

--- src/za/co/ntier/webform/sdr/viewmodel/DashboardAssessorsModeratorsVM.java
+++ src/za/co/ntier/webform/sdr/viewmodel/DashboardAssessorsModeratorsVM.java
@@ -92,7 +92,7 @@ public class DashboardAssessorsModeratorsVM {
 	}
 	
 	private void initList() {
-		Query assessorPersonQuery = MTable.get(Env.getCtx(), X_ZZAssessorPerson_v.Table_Name).createQuery("", null);
+		Query assessorPersonQuery = MTable.get(Env.getCtx(), X_ZZAssessorPerson_v.Table_Name).createQuery("CreatedBy=?", null);
 		assessorPersonQuery.setParameters(loginId);
 		assessors.clear();
 		assessors.addAll(assessorPersonQuery.list());
